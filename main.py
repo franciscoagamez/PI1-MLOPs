d5090984b0a5241a879fc095b6cc9ad96f4c7a8c
franciscoagamez <franciscoagamez@gmail.com>
2024-05-06 23:26:59 -0500

    Tercer commit
    se agregan mejoras al main.py en summary y description

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # In[ ]:
 
 
-from fastapi import FastAPI, HTTPException 
+from fastapi import FastAPI, HTTPException
 import traceback  # Importa la biblioteca para imprimir la traza de la pila
 from typing import List, Dict
 import pandas as pd
@@ -15,7 +15,7 @@ from functions import UsersNotRecommend
 from functions import PlayTimeGenre
 from functions import UserForGenre
 from functions import recomendacion_juego
-#from modelo_item_item import recomendacion_usuario
+
 
 
 # In[ ]:
@@ -24,18 +24,15 @@ from functions import recomendacion_juego
 # Crea una instancia de la aplicación FastAPI
 app = FastAPI()
 
-
-# In[ ]:
-
-@app.get("/")
-async def root():
-    return {"Mensaje": "Proyecto Individual"}
-
-# In[ ]:
+#class Item(BaseModel)
+    
 
 
-@app.get("/PlayTimeGenre/{genero}")
+@app.get("/PlayTimeGenre/{genero}", tags=['Endpoints'],
+         summary="Debe devolver año con mas horas jugadas para dicho género.",
+         description="Algunos de los géneros que puedes ingresar como ejemplo: Action, Adventure, Casual, Indie")
 async def user(genero: str):
+                                               
     try:
         result = PlayTimeGenre(genero)
         return result
@@ -49,7 +46,9 @@ async def user(genero: str):
 
 # In[ ]:   
 
-@app.get("/UserForGenre/{genero}")
+@app.get("/UserForGenre/{genero}",tags=['Endpoints'],
+         summary="Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.",
+         description="Algunos de los géneros que puedes ingresar como ejemplo: Action, Adventure, Casual, Indie, Simulation, Strategy")
 async def user(genero: str):
     try:
         result = UserForGenre(genero)
@@ -64,7 +63,9 @@ async def user(genero: str):
 
 # In[ ]:
 
-@app.get("/UsersRecommend/{year}")
+@app.get("/UsersRecommend/{year}",tags=['Endpoints'],
+         summary="Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado.",
+         description="Escriba años entre 2010 - 2015")
 async def user(year: int):
     try:
         year = int(year)
@@ -77,7 +78,9 @@ async def user(year: int):
         raise HTTPException(status_code=500, detail=f"Error al cargar el archivo UsersRecommend.csv: {str(e)}")
 # In[ ]:
 
-@app.get("/UsersNotRecommend/{year}")
+@app.get("/UsersNotRecommend/{year}",tags=['Endpoints'],
+         summary="Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado",
+         description="Escriba años entre 2010 - 2015")
 async def user(year: int):
     try:
         year = int(year)
@@ -90,7 +93,9 @@ async def user(year: int):
         raise HTTPException(status_code=500, detail=f"Error al cargar el archivo UsersNotRecommend.csv: {str(e)}")
     
 
-@app.get("/sentiment_analysis/{year}")
+@app.get("/sentiment_analysis/{year}",tags=['Endpoints'],
+         summary="Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.",
+         description="Puede insertar años entre 1989 hasta 2017")
 async def user(year: int):
     try:
         result = sentiment_analysis(year)
@@ -101,7 +106,9 @@ async def user(year: int):
         raise HTTPException(status_code=500, detail=f"Error al cargar el archivo UsersRecommend.csv: {str(e)}")
 
 
-@app.get("/recomendacion_juego/{item_id}", tags=['recomendacion_juego item_item'])
+@app.get("/recomendacion_juego/{item_id}", tags=['Modelo de recomendacion de video juegos item-item'],
+         summary="Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.",
+         description="Algunos ids de ejemplo: 761140, 643980, 670290, 772540, 658870, 733530")
 async def item(item_id: int):
 
     try:
